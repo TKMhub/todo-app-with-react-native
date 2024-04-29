d0424dc8da453a26b0751621354b0f9f414c5807
TKMhub <111867865+TKMhub@users.noreply.github.com>
2024-04-29 23:45:04 +0900

    dev

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [taskText, setTaskText] = useState("");
+  const [tasks, setTasks] = useState([]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Todo アプリ</Text>
+      <TextInput
+        placeholder="タスクを入力"
+        style={styles.input}
+        onChangeText={setTaskText}
+        value={taskText}
+      />
+      <TouchableOpacity style={styles.save} onPress={() => {}}>
+        <Text>追加</Text>
+      </TouchableOpacity>
+      <FlatList
+        data={[
+          { id: 1, title: "Todo 1" },
+          { id: 2, title: "Todo 2" },
+          { id: 3, title: "Todo 3" },
+        ]}
+      />
     </View>
   );
 }
@@ -13,8 +38,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 40,
+    padding: 40,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 10,
+    borderRadius: 6,
+    marginBottom: 10,
+  },
+  save: {
+    backgroundColor: "green",
+    padding: 10,
+    borderRadius: 6,
+    marginTop: 10,
   },
 });
